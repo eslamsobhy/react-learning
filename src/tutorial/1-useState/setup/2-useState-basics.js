457c68a8df81720c21d683b91759379b2020a7ac
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-07-06 23:29:59 +0200

    use state - basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+// GENERAL RULES OF HOOKS:
+//  - starts with 'use'
+//  - component name must be uppercase
+//  - must be in the function/component body
+//  - cannot call the hook conditionally
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("Hello world"));
+  // console.log(useState("hello")[0]);
+  // console.log(useState("hello")[1]);
+  // const [text, handler] = useState("normal text");
+  // console.log(text, handler);
+
+  const [title, setTitle] = useState("random title!");
+  const [count, setCount] = useState(0);
+
+  const counter = () => {
+    setCount(count + 1);
+  };
+
+  const clickHandler = () => {
+    if (title === "random title!") {
+      setTitle("new title!");
+    } else {
+      setTitle("random title!");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button className="btn" onClick={clickHandler}>
+        Change Title
+      </button>
+      <p>You clicked {count} times!</p>
+      <button className="btn" onClick={counter}>
+        Counter
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
