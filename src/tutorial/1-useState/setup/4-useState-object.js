86664575f783772c41778ccadbdb81f1acfe92a5
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-07-07 13:04:43 +0200

    use state - object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    message: "random message",
+  });
+  // console.log(person);
+
+  const changeMessage = () => {
+    // using the spread operator we are preserving the old data values,
+    //  and then we are overwriting the properties we want.
+
+    setPerson({ ...person, message: "new message" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button type="button" className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
