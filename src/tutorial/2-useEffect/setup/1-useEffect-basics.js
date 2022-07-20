f0543ef5e65650d77f32e8502698c3fb6845b903
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-07-20 17:20:14 +0200

    use effect - dependency list (the second parameter)

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
-// second parameter
+// second parameter => an array of dependencies
 
 // use effect is all about the work that is done out side of the component!
 // cannot use it conditionally!
@@ -12,7 +12,11 @@ const UseEffectBasics = () => {
     if (value > 0) {
       document.title = `New Messages(${value})`;
     }
-  });
+  }, [value]); //if the array left empty, then the use state will only called at the initial render!
+  // here we call it every time we change the value of the value state variable
+  useEffect(() => {
+    console.log("Hello World!");
+  }, []);
   console.log("Rendering the component!");
   return (
     <>
