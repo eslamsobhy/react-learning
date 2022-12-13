f183df977d1bc1116a874b2d654e3c4391d4464f
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-13 17:39:49 +0200

    made a separate file for the reducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,43 +4,7 @@ import { data } from "../../../data";
 import { useRef } from "react";
 import { useEffect } from "react";
 // reducer function
-const reducer = (state, action) => {
-  // console.log(state, action);
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [...state.people, action.payload];
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "Item Added",
-    };
-  }
-  if (action.type === "NO_VALUE") {
-    return {
-      ...state,
-      isModalOpen: true,
-      modalContent: "please enter value",
-    };
-  }
-  if (action.type === "CLOSE_MODAL") {
-    return {
-      ...state,
-      isModalOpen: false,
-    };
-  }
-  if (action.type === "REMOVE_ITEM") {
-    const newPeople = state.people.filter(
-      (person) => person.id !== action.payload
-    );
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "item removed",
-    };
-  }
-  throw new Error("No matching action type");
-};
+import { reducer } from "./reducer";
 
 const defaultState = {
   people: [],
