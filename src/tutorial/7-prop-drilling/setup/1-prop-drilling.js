e45b7ad6cc32afeb4d8689e6c87f69ae03f9b849
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-13 18:47:50 +0200

    applied prop drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -16,26 +16,32 @@ const PropDrilling = () => {
   return (
     <section>
       <h3>prop drilling</h3>
-      <List people={people} />
+      <List people={people} removePerson={removePerson} />
     </section>
   );
 };
 
-const List = ({ people }) => {
+const List = ({ people, removePerson }) => {
   return (
     <>
       {people.map((person) => {
-        return <SinglePerson key={person.id} {...person} />;
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+            removePerson={removePerson}
+          />
+        );
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name }) => {
+const SinglePerson = ({ id, name, removePerson }) => {
   return (
     <div className="item">
       <h4>{name}</h4>
-      <button>remove</button>
+      <button onClick={() => removePerson(id)}>remove</button>
     </div>
   );
 };
