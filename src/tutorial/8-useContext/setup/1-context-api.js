47970b80cdae4c14d36b6a2be74ea4789610ecea
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-14 18:20:51 +0200

    created another context

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -16,17 +16,28 @@ const ContextAPI = () => {
     });
   };
   return (
-    <PersonContext.Provider value={{ removePerson }}>
+    <PersonContext.Provider value={{ removePerson, people }}>
       <h3>prop drilling</h3>
-      <List people={people} />
+      <List />
     </PersonContext.Provider>
   );
 };
 
-const List = ({ people }) => {
+/*
+  actually in this commit we are just practicing, since there is no point of passing
+  the people array since it is used just one level far from the root component.
+  so we could just pass it to the List component like this:
+  <List people={people} />
+  and access it from the probs inside its component.
+*/
+
+const List = () => {
+  const mainData = useContext(PersonContext);
+  //the mainData has the removePerson method and the people array of objects (data)
+  // console.log(mainData);
   return (
     <>
-      {people.map((person) => {
+      {mainData.people.map((person) => {
         return <SinglePerson key={person.id} {...person} />;
       })}
     </>
