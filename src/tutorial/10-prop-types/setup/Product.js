119432a73a6dd8623555e329b7fcbb24ccca5549
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-15 16:48:51 +0200

    setup propTypes

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  console.log(image, name, price);
+  return (
+    <article className="product">
+      <h4>Single product</h4>
+      {/* <img src={image.url} alt={name} />
+      <h4>{name}</h4>
+      <p>${price}</p> */}
+    </article>
+  );
 };
 
 export default Product;
