d64dbeda021db4737a797fad479f4893dcbda544
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-07 16:52:55 +0200

    displayed the person according to the url param

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
 const Person = () => {
+  const { id } = useParams();
+  // console.log(params);
+  const person = data.find((item) => item.id === parseInt(id));
+
   return (
     <div>
-      <h2>person</h2>
+      <h2>{person.name}</h2>
     </div>
   );
 };
--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -12,10 +12,11 @@ import Navbar from "./Navbar";
 const ReactRouterSetup = () => {
   return (
     <Routes>
-      <Route exact path="/" element={<Home />}></Route>
-      <Route path="/about" element={<About />}></Route>
-      <Route path="/people" element={<People />}></Route>
-      <Route path="*" element={<Error />}></Route>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/people" element={<People />} />
+      <Route path="/people/:id" element={<Person />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 };
