c8a2cb7c61c4503b9c0348bc3b67b175daf7788c
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-07 17:16:15 +0200

    used the useNavigate hook to make the routing dynamic

--- src/tutorial/11-react-router/setup/People.js
+++ src/tutorial/11-react-router/setup/People.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { data } from "../../../data";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const People = () => {
   const [people, setPeople] = useState(data);
+  let navigate = useNavigate();
+
   return (
     <div>
       <h1>People Page</h1>
@@ -10,7 +12,13 @@ const People = () => {
         return (
           <div key={person.id} className="item">
             <h4>{person.name}</h4>
-            <Link to={`/people/${person.id}`}>more details</Link>
+            {/* <Link to={`/people/${person.id}`}>more details</Link> */}
+            <p
+              onClick={() => navigate(`/people/${person.id}`)}
+              style={{ color: "#2d87c8", cursor: "pointer" }}
+            >
+              more details
+            </p>
           </div>
         );
       })}
