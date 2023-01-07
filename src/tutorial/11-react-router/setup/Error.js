153fda238fdeff52e93b50bba3a132684ca5d5d2
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-07 16:41:29 +0200

    getting the location path name requested

--- src/tutorial/11-react-router/setup/Error.js
+++ src/tutorial/11-react-router/setup/Error.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const Error = () => {
+  let location = useLocation();
+  console.log(location);
+
   return (
     <div>
-      <h1>Error 404 Not Found</h1>
+      <h1>Error 404 Not Found!</h1>
+      <p>The page you requested ({location.pathname}) is not supported!</p>
       <Link to="/" className="btn">
         Back Home
       </Link>
