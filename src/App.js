286f18ba5226fb508bcb95dc04ac5d44f460a776
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-07 18:04:26 +0200

    displayed message for every render

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Setup from "./tutorial/11-react-router/setup";
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./tutorial/11-react-router/setup/Navbar";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import Navbar from "./tutorial/11-react-router/setup/Navbar";
 
 function App() {
   return (
     <div className="container">
-      <Router>
-        <Navbar />
-        <Setup />
-      </Router>
+      {/* <Router> */}
+      {/* <Navbar /> */}
+      <Setup />
+      {/* </Router> */}
     </div>
   );
 }
