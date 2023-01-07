63350c15ebc11481dd190ba5300766e82fc87fe7
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-07 18:17:01 +0200

    encountered the un-necassary re-rendering again!

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -10,6 +10,13 @@ const url = "https://course-api.com/javascript-store-products";
 const Index = () => {
   const { products } = useFetch(url);
   const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  // we encountered the same problem again, because react thinks that by calling the addToCart method
+  // we are recreating it again, thus the props changed, thus the component re-renders!
+  const addToCart = () => {
+    setCart(cart + 1);
+  };
 
   return (
     <>
@@ -17,12 +24,12 @@ const Index = () => {
       <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <BigList products={products} addToCart={addToCart} />
     </>
   );
 };
 
-const BigList = memo(({ products }) => {
+const BigList = memo(({ products, addToCart }) => {
   useEffect(() => {
     console.log("The big list rerendered");
   });
@@ -30,13 +37,19 @@ const BigList = memo(({ products }) => {
   return (
     <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>;
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}
+          ></SingleProduct>
+        );
       })}
     </section>
   );
 });
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   useEffect(() => {
     console.count("Single product rerendered!");
   });
@@ -50,6 +63,9 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button className="btn" onClick={addToCart}>
+        Add To Cart
+      </button>
     </article>
   );
 };
